ee1198ce67ca1693b00b24d82f406daeeaf8ac1f
Miguel <miguelmail2006@gmail.com>
2024-01-20 21:38:06 -0300

    react-challenge - Add some look and feel basics

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'breaking-bad': 'rgb(var(--color-breaking-bad))',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
